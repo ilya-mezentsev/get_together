825554d1b4845124c431269a071e829eb21c7c65
imezentsev <ilyamezentsev41@gmail.com>
2020-01-28 12:58:48 +0300

    Added credentials repository and configuration of docker for running golang tests into container

---
+++ backend/src/mock/repositories/utils.go
@@ -0,0 +1,120 @@
+package repositories
+
+import "github.com/jmoiron/sqlx"
+
+const (
+  DropTablesQuery = `
+  DROP TABLE IF EXISTS users CASCADE;
+  DROP TABLE IF EXISTS info;
+  DROP TABLE IF EXISTS rating;
+  DROP TABLE IF EXISTS meetings CASCADE;
+  DROP TABLE IF EXISTS meetings_settings;
+  DROP TABLE IF EXISTS meetings_places;
+  DROP TABLE IF EXISTS chats CASCADE;
+  DROP TABLE IF EXISTS meetings_messages;
+  DROP TABLE IF EXISTS meetings_request_messages;
+  DROP TYPE IF EXISTS GENDER;
+  DROP TYPE IF EXISTS MEETING_STATUS;
+  DROP TYPE IF EXISTS CHAT_TYPE;
+  DROP TYPE IF EXISTS CHAT_STATUS;`
+  CreateTablesQuery = `
+  CREATE TYPE GENDER AS ENUM('male', 'female');
+  CREATE TYPE MEETING_STATUS AS ENUM('pending', 'archived');
+  CREATE TYPE CHAT_TYPE AS ENUM('meeting', 'meeting_request');
+  CREATE TYPE CHAT_STATUS AS ENUM('chatting', 'archived');
+
+  CREATE TABLE IF NOT EXISTS users(
+    id SERIAL PRIMARY KEY,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(32) NOT NULL
+  );
+
+  CREATE TABLE IF NOT EXISTS info(
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    name VARCHAR(255) NOT NULL,
+    nickname VARCHAR(255) NOT NULL,
+    gender GENDER DEFAULT NULL,
+    age INTEGER DEFAULT NULL,
+    avatar_url VARCHAR DEFAULT NULL
+  );
+
+  CREATE TABLE IF NOT EXISTS rating(
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    value FLOAT NOT NULL,
+    tag VARCHAR(100) NOT NULL
+  );
+
+  CREATE TABLE IF NOT EXISTS meetings(
+    id SERIAL PRIMARY KEY,
+    admin_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    user_ids INTEGER[] DEFAULT NULL,
+    status MEETING_STATUS DEFAULT 'pending',
+    archived_at TIMESTAMP DEFAULT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+  );
+
+  CREATE TABLE IF NOT EXISTS meetings_settings(
+    id SERIAL PRIMARY KEY,
+    meeting_id INTEGER NOT NULL REFERENCES meetings(id) ON DELETE CASCADE,
+    title VARCHAR(255) NOT NULL,
+    max_users INTEGER DEFAULT NULL,
+    tags VARCHAR(100)[] DEFAULT NULL,
+    date_time TIMESTAMP NOT NULL,
+    description TEXT DEFAULT '',
+    duration INTEGER DEFAULT NULL,
+    min_age INTEGER DEFAULT NULL,
+    gender GENDER DEFAULT NULL,
+    request_description_required BOOLEAN DEFAULT FALSE
+  );
+
+  CREATE TABLE IF NOT EXISTS meetings_places(
+    id SERIAL PRIMARY KEY,
+    meeting_id INTEGER NOT NULL REFERENCES meetings(id) ON DELETE CASCADE,
+    label VARCHAR(511) NOT NULL,
+    latitude FLOAT NOT NULL,
+    longitude FLOAT NOT NULL
+  );
+
+  CREATE TABLE IF NOT EXISTS chats(
+    id SERIAL PRIMARY KEY,
+    meeting_id INTEGER NOT NULL REFERENCES meetings(id) ON DELETE CASCADE,
+    type CHAT_TYPE NOT NULL,
+    status CHAT_STATUS DEFAULT 'chatting',
+    archived_at TIMESTAMP DEFAULT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+  );
+
+  CREATE TABLE IF NOT EXISTS meetings_messages(
+    id SERIAL PRIMARY KEY,
+    chat_id INTEGER NOT NULL REFERENCES chats(id) ON DELETE CASCADE,
+    meeting_id INTEGER NOT NULL REFERENCES meetings(id) ON DELETE CASCADE,
+    sender_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    text TEXT NOT NULL,
+    sending_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+  );
+
+  CREATE TABLE IF NOT EXISTS meetings_request_messages(
+    id SERIAL PRIMARY KEY,
+    chat_id INTEGER NOT NULL REFERENCES chats(id) ON DELETE CASCADE,
+    admin_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    sender_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    text TEXT NOT NULL,
+    sending_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+  );`
+)
+
+func dropTables(db *sqlx.DB) {
+  _, err := db.Exec(DropTablesQuery)
+  if err != nil {
+    panic(err)
+  }
+}
+
+func initTables(db *sqlx.DB) {
+  _, err := db.Exec(CreateTablesQuery)
+  if err != nil {
+    panic(err)
+  }
+}
--- scripts/go_tests.sh
+++ scripts/go_tests.sh
@@ -9,28 +9,12 @@ if [[ ${REPORT_FOLDER}/ != ${GOPATH}* ]]; then
   exit 1
 fi
 
-folders=()
-function setFoldersWithTests() {
-  cd ${GOPATH}/src
-
-  for dir in $(find . -type d)
-  do
-    # should skip github libs
-    if [[ ${dir} == *github* ]]; then
-      continue
-    fi
-    if tests=$(find ${GOPATH}/src/${dir} -maxdepth 1 -name '*_test.go'); [[ ${tests} != "" ]]; then
-      folders+=(${dir})
-    fi
-  done
-}
-
+source ${PROJECT_ROOT}/scripts/_set_tests_folders.sh
 rm -rf ${REPORT_FOLDER}/*
-setFoldersWithTests
 for dir in "${folders[@]}"
 do
   reportFileName=$(echo -n ${dir} | md5sum | awk '{print $1}')
-  cd ${GOPATH}/src/${dir} && TESTING_MODE=1 go test -coverprofile=${REPORT_FOLDER}/${reportFileName}.out
+  cd ${GOPATH}/src/${dir} && go test -coverprofile=${REPORT_FOLDER}/${reportFileName}.out
   if [[ $1 = html ]]; then # open reports in browser
     go tool cover -html=${REPORT_FOLDER}/${reportFileName}.out
   fi
