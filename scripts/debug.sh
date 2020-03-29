21251f5a542849de2044ec1ab720f61705c90120
Ilya <ism20122@ya.ru>
2020-03-29 20:21:05 +0300

    Paths to source code (frontend and api) inside containers were moved to env file

--- scripts/debug.sh
+++ scripts/debug.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 export RUN_GO_COMMAND="go run main.go"
 export RUN_FRONTEND_COMMAND="npm run start"
 export NGINX_DOCKERFILE="Dockerfile.debug"
--- scripts/stage.sh
+++ scripts/stage.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 export RUN_GO_COMMAND="go run main.go"
 export RUN_FRONTEND_COMMAND="echo hello world"
 export NGINX_DOCKERFILE="Dockerfile.stage"
